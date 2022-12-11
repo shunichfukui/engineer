e876f4f61b583283444330ab646d4f7e37a9d3c4
shunich <fukusyun3@gmail.com>
2022-12-11 09:14:24 +0900

    feat: tailwindcss setting

--- front/app/pages/_app.tsx
+++ front/app/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
+import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
